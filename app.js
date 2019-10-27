72e5900738dfaa5aaa1140c0e513281ab790b282
Morgan Murrah <morgan@easyrxcloud.com>
2019-10-26 23:36:14 -0700

    Add link

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ class Wrapper {
     userIdx: {}
   })).element);
 
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   const shuffleArray = function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
