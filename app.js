7b0cc758d80a67f1fe547139e9c07bff831ef807
Morgan Murrah <morgan@easyrxcloud.com>
2019-10-26 21:57:17 -0700

    ES5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 
-const app = document.getElementById("app");
-window.run = () => app.innerText="Fun!";
+var app = document.getElementById("app");
+window.run = function() { app.innerText="Fun!"; };
 app.innerHTML = '<button onclick="run()">Load</button>';
